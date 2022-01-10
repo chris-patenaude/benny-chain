c99053c2300fff0a3e95f99c3fbf1372e3f2cf16
Chris Patenaude <patenauc@oregonstate.edu>
2022-01-10 11:20:30 -0800

    Comment Blockchain class

--- Blockchain.js
+++ Blockchain.js
@@ -1,10 +1,18 @@
 const Block = require("./Block");
 
 class Blockchain {
+
+	/**
+	 * Initializes an new Blockchain
+	 */
     constructor() {
         this.chain = [Block.genesis()];
     }
 
+	/**
+	 * Initializes and adds a new block to the chain
+	 * @param {*} data 
+	 */
     addBlock(data) {
         const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
         this.chain.push(block);
