dd148065bc1c01212825495bb02d575d723fb567
Chris Patenaude <patenauc@oregonstate.edu>
2022-01-11 16:39:00 -0800

    Handle peer-to-peer connections

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("../blockchain");
+const P2PServer = require("./p2p-server");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const blockchain = new Blockchain();
+const p2pServer = new P2PServer(blockchain);
 
 app.use(bodyParser.json());
 
@@ -19,3 +21,4 @@ app.post("/mine", (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,23 +1,63 @@
 const Websocket = require("ws");
+const Blockchain = require("../blockchain");
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
+
+// comma separated list of peer addresses (e.g. "wp://localhost:3002,wp://localhost:3003,ect.")
 const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
 
 class P2PServer {
+    /**
+     * Creates an instance of a peer-to-peer server to facilitate blockchain synchronization
+     * @param {Blockchain} blockchain The block chain being connected manipulated
+     */
     constructor(blockchain) {
         this.blockchain = blockchain;
         this.sockets = [];
     }
 
+    /**
+     * Start the server and listen for new connections
+     */
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on("connection", (socket) => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for peer-to-peer connection on port: ${P2P_PORT}`);
     }
 
+    /**
+     * Connect to each peer address
+     */
+    connectToPeers() {
+        peers.forEach((peer) => {
+            const socket = new Websocket(peer);
+            socket.on("open", () => this.connectSocket(socket));
+        });
+    }
+
+    /**
+     * Add a new socket connection
+     * @param {Websocket} socket Websocket to be added to the pool
+     */
     connectSocket(socket) {
         this.sockets.push(socket);
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
         console.log("Socket connected");
+    }
+
+    /**
+     * Add a message listener to a socket
+     * @param {Websocket} socket Listener target
+     */
+    messageHandler(socket) {
+        socket.on("message", (message) => {
+            const data = JSON.parse(message);
+            console.log("Data:", data);
+        });
     }
 }
 
